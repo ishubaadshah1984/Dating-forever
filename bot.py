fab227a25a4a14a8730d2e18f25eef047376ab05
ishubaadshah1984 <ishubaadshah1984@gmail.com>
2026-09-09 17:04:06 +0530

    Change asyncio.run to new event loop setup

--- bot.py
+++ bot.py
@@ -159,4 +159,6 @@ def main():
     app.run_polling()
     
 if __name__ == "__main__":
-    asyncio.run(main())
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(main())
