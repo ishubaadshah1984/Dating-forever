6b10d2239ca5380ead354c49809b70e63f3a9f4f
ishubaadshah1984 <ishubaadshah1984@gmail.com>
2026-09-09 15:50:19 +0530

    Fix logging import statement in bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-CallbackQueryHanInlineKeyboardMarkupr logging
+logging logging
 import os
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Application, CommandHandler, CallbackQueryHandler, MessageHandler, filters
