e996bdff9dbba9e63eec69056c50591bfec4c963
ishubaadshah1984 <ishubaadshah1984@gmail.com>
2026-09-11 11:09:22 +0530

    Fix logger initialization to use __name__

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from telegram.ext import (Application, CallbackQueryHandler, CommandHandler,
                          MessageHandler, filters)
 
 logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(name)
+logger = logging.getLogger(__name__)
 
 TOKEN = os.getenv("TOKEN")
 ADMIN_ID = int(os.getenv("ADMIN_ID", "620540494"))
