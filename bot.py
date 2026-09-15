479444be4842ebf82886ada738ce5d7c3183cb1c
ishubaadshah1984 <ishubaadshah1984@gmail.com>
2026-09-15 16:48:53 +0530

    Set default value for ADMIN_ID from environment

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ from telegram.ext import Application, CallbackQueryHandler, CommandHandler, Mess
 logging.basicConfig(level=logging.INFO)
 
 TOKEN = os.getenv("TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID"))
+ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
 
 DATA_FILE = "users.json"
 
