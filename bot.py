f4be4811e7bc5a8dfabf84586371845d39cf005e
ishubaadshah1984 <ishubaadshah1984@gmail.com>
2026-09-15 16:55:01 +0530

    Add msg_map to save_users function

--- bot.py
+++ bot.py
@@ -106,6 +106,7 @@ def save_users():
             }, f, ensure_ascii=False)
     except Exception as e:
         print("save_users failed:", e)
+      
 def anon_name(uid):
     return f"User_{str(uid)[-4:]}"
 
